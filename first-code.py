d793278d1663738379fb1df875dc3b417441616b
lodavide <daviderich.loetscher@uzh.ch>
2026-09-23 12:30:29 +0200

    Add files via upload

---
+++ first-code.py
@@ -0,0 +1,10 @@
+Mates_Ive_drunk_today = 1
+
+if Mates_Ive_drunk_today > 2:
+    print("I'm sooo stressed, the exams are just around the corner")
+elif Mates_Ive_drunk_today > 1:
+    print("I rly haven't slept a lot")
+else:
+    print("I'm so relaxed")
+
+
